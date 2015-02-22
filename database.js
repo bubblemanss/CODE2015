0743daa73e7be50cba4040c5e32e2c55343410d8
Tyrone <ty.cx@hotmail.com>
2015-02-22 15:53:08 -0500

    Made callback optional for some database calls
    Also updated the documentation for all the functions

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 var mongojs = require('mongojs');
-const dburl = "mongodb://localhost:8080"  //TODO: replace this with the real url
+const dburl = "mongodb://localhost:27017"  //TODO: replace this with the real url
 var db = mongojs(dburl);
-var UserDB = mongojs.collection('users'); // contains both normal and investors
+var UserDB = db.collection('users'); // contains both normal and investors
 
-// username - string
-// password - string
-// callback - function(User)
+// username: string
+// password: string
+// callback: function(user: User)
 exports.SearchUser = function(username, password, callback) {
   UserDB.findOne({
     'username': username,
@@ -13,70 +13,80 @@ exports.SearchUser = function(username, password, callback) {
   }, function(err, user) {
     if (err){
       console.log(err);
+      callback(null)
     }
-    return callback(user);
+    callback(user);
   });
 }
 
-// username - string
-// callback - function(exists)
+// username: string
+// callback: function(exists: boolean)
 exports.UsernameExists = function(username, callback) {
   //faster to user find as opposed to findOne?
   UserDB.find({
     'username': username,
   }, function(err, cursor) {
     if (!err && cursor)
-      return callback(true);
-    return callback(false);
+      callback(true);
+    callback(false);
   });
 }
 
-// user - User object
-// isInvestor - boolean
-// callback - function(successful)
+// user: User object
+// isInvestor: boolean
+// callback: function(successful: boolean)
 exports.CreateUser = function(user, callback) {
   //note: if we create a specific _id for each user based on their
   // username and pass, then we can simplify this next part a bit
   // by just attempting an insert (which will fail if _id already exists).
   UsernameExists(user.username, function(exists) {
-    if (exists) {
-      return callback(false);
+    if (callback && exists) {
+      callback(false);
     } else {
       //Add the user to the database
       UserDB.insert(user)
-      return callback(true);
+      if (callback) callback(true);
     }
   });
 }
 
+// username: string
+// password: string
+// callback: function(err: string)
+//  err will be the empty string if there was no error
+exports.DeleteUser = function(username, password, callback) {
+  query = {
+    'username': username,
+    'password': password
+  }
 
-exports.Query = function(query, callback) {
-
+  UserDB.remove(query, function(err, numRemoved){
+    if (!err) {
+      if (numRemoved > 1)
+        err = "Removed more than one user"
+      if (numRemoved == 0)
+        err = "No users removed"
+    }
+    if (err) console.log(err);
+    if (callback) callback(err)
+  })
 }
 
-
-/*
-function userToDoc(user, callback) {
-  callback({
-    'username': user.username,
-    'password': user.password,
-    'email': user.email,
-    'first': user.firstname,
-    'last': user.lastname,
-    'industry': user.industry,
-    'resume': user.resume,
-    'sites': user.sites,
-    'investor': false
-  });
+// filter: map
+// callback: function(document: DB Document)
+//  The document for the callback should be a user object if you are searching
+//  for a user. It may be other stuff if you are searching in the Online DBs
+exports.Query = function(filter, callback) {
+  query = {
+    'username': filter['user'],
+    'password': filter['pass'],
+    'email': filter['email'],
+    'first': filter['first'],
+    'last': filter['last'],
+    'industry': filter['industry'],
+    'resume': filter['resume'],
+    'links': filter['links'],
+    'type': filter['type']
+  }
+  UserDB.find(query, callback);
 }
-function invToDoc(user, callback) {
-  callback({
-    'username': user.username,
-    'password': user.password,
-    'email': user.email,
-    'company': user.company,
-    'sites': user.sites,
-    'investor': true
-  });
-}
-*/
